e208d1fa9a9f5cb9a97a2eac16b6984f4dee96db
jot1x4k <juanoroprada4@gmail.com>
2026-05-11 16:30:41 -0500

    correccion reschedule

--- builder/src/main/java/com/jrodriguez/RescheduledAppointmentBuilder.java
+++ builder/src/main/java/com/jrodriguez/RescheduledAppointmentBuilder.java
@@ -2,54 +2,74 @@ package com.jrodriguez;
 
 public class RescheduledAppointmentBuilder extends AppointmentBuilder {
 
+    private Appointment originalAppointment;
+    private java.sql.Date newDate;
+    private java.sql.Time newTime;
+    private String newStatus;
+
+    public void setOriginalAppointment(Appointment originalAppointment) {
+        this.originalAppointment = originalAppointment;
+        if (originalAppointment != null) {
+            appointment.setPatient_document(originalAppointment.getPatient_document());
+            appointment.setPatient_name(originalAppointment.getPatient_name());
+            appointment.setPatient_phone(originalAppointment.getPatient_phone());
+            appointment.setProfessional_id(originalAppointment.getProfessional_id());
+            appointment.setProfessional_name(originalAppointment.getProfessional_name());
+            appointment.setDuration_minutes(originalAppointment.getDuration_minutes());
+            appointment.setReason(originalAppointment.getReason());
+        }
+    }
+
     @Override
     public void buildPatientDocument(String document) {
-        appointment.setPatient_document(document);
+        throw new UnsupportedOperationException("No se puede modificar el documento del paciente en reagendamiento");
     }
 
     @Override
     public void buildPatientName(String name) {
-        appointment.setPatient_name(name);
+        throw new UnsupportedOperationException("No se puede modificar el nombre del paciente en reagendamiento");
     }
 
     @Override
     public void buildPatientPhone(String phone) {
-        appointment.setPatient_phone(phone);
+        throw new UnsupportedOperationException("No se puede modificar el teléfono del paciente en reagendamiento");
     }
 
     @Override
     public void buildProfessionalId(Long id) {
-        appointment.setProfessional_id(id);
+        throw new UnsupportedOperationException("No se puede modificar el profesional en reagendamiento");
     }
 
     @Override
     public void buildProfessionalName(String name) {
-        appointment.setProfessional_name(name);
+        throw new UnsupportedOperationException("No se puede modificar el nombre del profesional en reagendamiento");
     }
 
     @Override
     public void buildAppointmentDate(java.sql.Date date) {
+        this.newDate = date;
         appointment.setAppointment_date(date);
     }
 
     @Override
     public void buildAppointmentTime(java.sql.Time time) {
+        this.newTime = time;
         appointment.setAppointment_time(time);
     }
 
     @Override
     public void buildDurationMinutes(Integer duration) {
-        appointment.setDuration_minutes(duration);
+        throw new UnsupportedOperationException("No se puede modificar la duración en reagendamiento");
     }
 
     @Override
     public void buildReason(String reason) {
-        appointment.setReason(reason);
+        throw new UnsupportedOperationException("No se puede modificar el motivo en reagendamiento");
     }
 
     @Override
     public void buildStatus(String status) {
+        this.newStatus = status;
         appointment.setStatus(status);
     }
-
 }
